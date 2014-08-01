daea041f8d1ab076b3dfb9719341da8c65bec853
Bing Gu <bingg@email.sc.edu>
2014-08-01 16:55:07 -0400

    add file

---
+++ helium_mpi/1.4.6/mod.f90
@@ -0,0 +1,11 @@
+      module cdat
+
+      implicit none
+      
+!      integer*4, public, parameter  :: 
+      real*8, public, parameter  :: pi = 4.d0*atan(1.d0), bin_pdf = 1d-2, & 
+                                    binvrs_pdf = 1d2 
+
+      save 
+      
+      end module cdat
